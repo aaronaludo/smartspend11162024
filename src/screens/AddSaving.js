4f12b37fb9046f121e489404152b4a93c333db0b
Aaron Vincent Aludo <aaronvincentaludo23@gmail.com>
2024-11-16 01:11:21 +0800

    update

---
+++ src/screens/AddSaving.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const AddSaving = () => {
+  return (
+    <View>
+      <Text>Add Saving</Text>
+    </View>
+  );
+};
+
+export default AddSaving;
